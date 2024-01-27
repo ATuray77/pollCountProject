ff200568f7c9e8377dd244c04005cb79c3be60b2
Aruna Turay <tura_arun@yahoo.com>
2024-01-27 15:26:47 -0500

    adds eventlistener to the form with a submit function that collectes the votes after running some validation

--- script.js
+++ script.js
@@ -4,3 +4,20 @@ poll.set('Vue', 0);
 poll.set('Angular', 0);
 poll.set('Svelt', 0);
 poll.set('Other', 0);
+
+function submitForm(e) {
+    e.preventDefault();
+
+    const selectedOption = document.querySelector("input[name='poll-option']:checked")
+
+    if (!selectedOption) { //input validation
+        alert('Please Select an Option');
+        return;
+    }
+
+    let voteCount = //using the get method on our map
+};
+
+
+
+document.getElementById('poll-form').addEventListener('submit', submitForm)
