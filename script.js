0d3dfd621a8f845bf49fdf78c6a4132ff590056e
Aruna Turay <tura_arun@yahoo.com>
2024-01-27 15:45:39 -0500

    adds display functionality

--- script.js
+++ script.js
@@ -15,9 +15,28 @@ function submitForm(e) {
         return;
     }
 
-    let voteCount = //using the get method on our map
+    let voteCount = poll.get(selectedOption.value)//using the get method on our map, to pull out the value of the selected option
+    poll.set(selectedOption.value, voteCount + 1)  //running set() method on the map. After getting the value, add that to the vote count
+    
+    //display the result
+    displayResults();
 };
 
+function displayResults() {
+    const results = document.getElementById('results')//grabs the results div
+    results.innerHTML = ' '; //clears the result when this runs initially
+    for (const [option, votes] of poll) { //running a forOf loop here to display the results
+        const optionElement = document.createElement('div');
+        optionElement.classList.add(  //adds bootstrap classes
+            'border-bottom', //border
+            'p-2', //padding
+            'd-flex', //flex box
+            'justify-content-between'
+            );
 
+            optionElement.innerHTML = `<strong>${option}: </strong> ${votes} Votes`
+            results.appendChild(optionElement)//adds that to the DOM
+    }
+}
 
 document.getElementById('poll-form').addEventListener('submit', submitForm)
